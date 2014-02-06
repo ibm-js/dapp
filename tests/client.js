d4f8344282cc0a3da84bb9e8fda021019bc3d5e1
Christophe Jolif <cjolif@gmail.com>
2014-02-06 17:53:39 +0100

    add intern test skelton

---
+++ tests/client.js
@@ -0,0 +1,17 @@
+// Test file to run infrastructure tests from a browser
+// Run using http://localhost/dapp/node_modules/intern/client.html?config=tests/client
+
+define({
+	loader: {
+		// location of all the packages, relative to client.html
+		baseUrl: "../../.."
+	},
+
+	useLoader: {
+		"host-node": "requirejs",
+		"host-browser": "../../../requirejs/require.js"
+	},
+
+	// Non-functional test suites
+	suites: [ "dapp/tests/unit/all" ]
+});
