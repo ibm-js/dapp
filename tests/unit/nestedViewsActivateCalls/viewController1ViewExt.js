e909fe671333e45709e16c3070385fac36740f07
Ed Chatelain <ed.chatelain@gmail.com>
2014-08-15 15:35:24 -0400

    Add one test which uses a custom view, I should add more, but this is a start.

---
+++ tests/unit/nestedViewsActivateCalls/viewController1ViewExt.js
@@ -0,0 +1,54 @@
+// jshint quotmark:false
+define(["dojo/dom", "dojo/on", "delite/register", "dcl/dcl", "dapp/View"],
+	function (dom, on, register, dcl, View) {
+		return dcl([View], {
+				//	define([ /*"dojo/dom", "dojo/on", "delite/register", "dcl/dcl", "dapp/View"*/ ],
+				//	function ( /*dom, on, register, dcl, View*/ ) {
+				//return {
+				name: "",
+				beforeActivateCallCount: 0,
+				beforeDeactivateCallCount: 0,
+				afterActivateCallCount: 0,
+				afterDeactivateCallCount: 0,
+				init: function () {
+					if (this.domNode) {
+						this.domNode.name = this.id;
+					} else {
+						this.name = this.id;
+					}
+				},
+				//beforeActivate: function ( /*previousView, viewData*/ ) {
+				//	//console.log("app-view:", "beforeActivate called for [" + this.viewName +
+				//	//"] with previousView.id =[" + (previousView ? previousView.id : "") + "] with viewData=",
+				//  //viewData);
+				//	this.beforeActivateCallCount++;
+				//},
+				beforeActivate: dcl.superCall(function (sup) {
+					return function () {
+						this.beforeActivateCallCount++;
+						return sup.call(this);
+					};
+				}),
+
+				beforeDeactivate: function ( /*nextView, viewData*/ ) {
+					//console.log("app-view:", "beforeDeactivate called for [" + this.viewName +
+					//	"] with previousView.id =[" + (nextView ? nextView.id : "") + "]");
+					this.beforeDeactivateCallCount++;
+				},
+				afterActivate: function ( /*previousView, viewData*/ ) {
+					//console.log("app-view:", "afterActivate called for [" + this.viewName +
+					// "] with previousView.id =[" +
+					//	(previousView ? previousView.id : "") + "] with viewData=", viewData);
+					this.afterActivateCallCount++;
+				},
+				afterDeactivate: function ( /*nextView, viewData*/ ) {
+					//console.log("app-view:", "afterDeactivate called for [" + this.viewName +
+					//	"] with previousView.id =[" + (nextView ? nextView.id : "") + "]");
+					this.afterDeactivateCallCount++;
+				},
+				destroy: function () {
+					//console.log("app-view:", " in [" + this.viewName + "] destroy called for [" + this.id + "]");
+				}
+			} //;
+		); // for dcl view attempt...
+	});
