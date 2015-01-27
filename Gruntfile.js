c281df40f9597ac25f97f8f9cf5f0922817707c1
Ed Chatelain <ed.chatelain@gmail.com>
2015-01-27 16:06:52 -0500

    Update dapp to use the CustomRunner to show tests Started and Skipped, and fix bower.json for pointer and emca402.

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,14 +63,14 @@ module.exports = function (grunt) {
 				options: {
 					runType: "runner",
 					config: "tests/intern.local",
-					reporters: ["runner"]
+					reporters: [] // pass :runner to avoid logging STARTED & SKIPPED
 				}
 			},
 			remote: {
 				options: {
 					runType: "runner",
 					config: "tests/intern",
-					reporters: ["runner"]
+					reporters: [] // pass :runner to avoid logging STARTED & SKIPPED
 				}
 			}
 		},
@@ -127,12 +127,18 @@ module.exports = function (grunt) {
 
 	// By default, lint and run all tests.
 	grunt.registerTask("default", ["jsbeautifier", "jshint"]);
+
+	// Testing.
 	// always specify the target e.g. grunt test:remote, grunt test:remote
-	// then add on any other flags afterwards e.g. console, lcovhtml
+	// then add on any other flags afterwards e.g. runner, console, lcovhtml
 	var testTaskDescription = "Run this task instead of the intern task directly! \n" +
 		"Always specify the test target e.g. \n" +
 		"grunt test:local\n" +
+		"grunt test:local.android\n" +
+		"grunt test:local.ios\n" +
 		"grunt test:remote\n\n" +
+		"To override the default reporter 'customRunner' with the 'runner' reporter pass the runner flag e.g. \n" +
+		"grunt test:local:runner\n" +
 		"Add any optional reporters via a flag e.g. \n" +
 		"grunt test:local:console\n" +
 		"grunt test:local:lcovhtml\n" +
@@ -150,15 +156,19 @@ module.exports = function (grunt) {
 		if (this.flags.lcovhtml) {
 			addReporter("lcovhtml");
 		}
-		// the lcov option will generate the lcov.info file which may be able to be used with genhtml for coverage info.
-		if (this.flags.lcov) {
-			addReporter("lcov");
+
+		if (this.flags.runner) {
+			addReporter("runner");
 		}
 
 		if (this.flags.console) {
 			addReporter("console");
 		}
+
+		if (!this.flags.runner || this.flags.customRunner) {
+			addReporter("dapp/tests/customRunner");
+		}
+
 		grunt.task.run("intern:" + target);
 	});
-
 };
